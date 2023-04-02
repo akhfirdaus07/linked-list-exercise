10b2a8b324528e7a2e927430a8fde2aa2cbf50ef
akhfirdaus07 <121778146+akhfirdaus07@users.noreply.github.com>
2023-04-02 16:15:52 +0700

    Add contains value function

--- LinkedList.js
+++ LinkedList.js
@@ -46,13 +46,34 @@ const LinkedList = ()=>{
         if(isNaN(index) || index<1) return console.log("Input must be a number and greater than 0");
         if(HEAD===null) return console.log("List is empty");
         let pointer=HEAD;
-        for(let i=1;i<index;i++){
-            pointer=pointer.nextNode;
-        };
+        for(let i=1;i<index;i++) pointer=pointer.nextNode;
         console.log(pointer);
+        return pointer;
     };
 
-    return { append, prepend, size, head, tail, at };
+    const pop=()=>{
+        if(HEAD===null) return console.log("List is empty");
+        if(HEAD.nextNode===null) HEAD=null;
+        
+        at(length-1).nextNode=null;
+        length--;
+        console.log(`Success delete the last node`);
+    };
+
+    const find=(values)=>{
+        if(HEAD===null) return console.log("List is empty");
+        let pointer=HEAD;
+        for(let i=1;i<=length;i++){
+            if(pointer.value==values){
+                return console.log(true)
+            } else {
+                pointer=pointer.nextNode;
+            }
+        };
+        console.log(false);
+    }
+
+    return { append, prepend, size, head, tail, at, pop, find };
 };
 
 const list=LinkedList();
@@ -70,3 +91,11 @@ list.tail();
 list.at("satu");
 list.at(-1);
 list.at(3);
+
+list.pop();
+
+list.find("node5");
+list.find("node4");
+list.find("node3");
+list.find("node2");
+list.find("node1");
