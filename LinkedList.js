bf977eba794025265bb262808f4248f4b1b9bff3
akhfirdaus07 <121778146+akhfirdaus07@users.noreply.github.com>
2023-04-02 15:58:02 +0700

    Add at index function

--- LinkedList.js
+++ LinkedList.js
@@ -42,14 +42,31 @@ const LinkedList = ()=>{
         console.log(pointer.value);
     };
 
+    const at=(index)=>{
+        if(isNaN(index) || index<1) return console.log("Input must be a number and greater than 0");
+        if(HEAD===null) return console.log("List is empty");
+        let pointer=HEAD;
+        for(let i=1;i<index;i++){
+            pointer=pointer.nextNode;
+        };
+        console.log(pointer);
+    };
 
-    return { append, prepend, size, head, tail };
+    return { append, prepend, size, head, tail, at };
 };
 
 const list=LinkedList();
 
-list.append("node2");
+list.append("node3");
+list.append("node4");
+list.append("node5");
+list.prepend("node2");
 list.prepend("node1");
+
 list.size();
 list.head();
 list.tail();
+
+list.at("satu");
+list.at(-1);
+list.at(3);
