9794661dfaf7a3a2d0b3e03b4884656874f4067a
akhfirdaus07 <121778146+akhfirdaus07@users.noreply.github.com>
2023-04-02 15:49:30 +0700

    Add size head and tail to factory function Linkedlist

--- LinkedList.js
+++ LinkedList.js
@@ -1,4 +1,55 @@
-import Node from "./Node";
+const Node = (input)=>{
+    return {
+        value: input || null, 
+        nextNode:null,
+    };
+};
 
 const LinkedList = ()=>{
+    let HEAD=null;
+    let length=0;
+
+    const append=(value)=>{
+        const newNode=Node(value);
+        length++;
+        if(HEAD===null) return HEAD=newNode;
+        let pointer=HEAD;
+        while(pointer.nextNode !== null) pointer=pointer.nextNode;
+        pointer.nextNode=newNode;
+    };
+    const prepend=(value)=>{
+        const newNode=Node(value);
+        length++;
+        if(HEAD===null) return HEAD=newNode;
+        newNode.nextNode=HEAD;
+        HEAD=newNode;
+    };
+
+    const size=()=>{
+        if(HEAD===null) return console.log("List is empty");
+        console.log(length);
+    };
+
+    const head=()=>{
+        if(HEAD===null) return console.log("List is empty");
+        console.log(HEAD.value);
+    };
+
+    const tail=()=>{
+        if(HEAD===null) return console.log("List is empty");
+        let pointer=HEAD;
+        while(pointer.nextNode!==null) pointer=pointer.nextNode;
+        console.log(pointer.value);
+    };
+
+
+    return { append, prepend, size, head, tail };
 };
+
+const list=LinkedList();
+
+list.append("node2");
+list.prepend("node1");
+list.size();
+list.head();
+list.tail();
